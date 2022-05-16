74bcaafcfaf230873c1f4284edc42012122c28c5
fawaikuangtu <wanchengwei001@gmail.com>
2022-05-16 21:20:06 +0800

    测试重置分支

---
+++ src/views/test/index.vue
@@ -0,0 +1,26 @@
+<template>
+  的观点
+</template>
+  
+<script>
+import { reactive, toRefs, defineComponent, onMounted, getCurrentInstance } from 'vue';
+  
+export default defineComponent({
+  name: 'df-df',
+  components: {},
+  props: {},
+  setup(props, context) {
+    const internalInstance = getCurrentInstance(); 
+    const self = internalInstance.proxy
+    const state = reactive({})
+    onMounted(() => {})
+    return {
+      ...toRefs(state)
+    }
+  }
+})
+</script>
+  
+<style lang="scss" scoped>
+  
+</style>
